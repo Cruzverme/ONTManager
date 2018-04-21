f1f6b13b21932ac2860df01ce055b7a415543c2f
Cruzverme <grossmino@hotmail.com>
2018-04-21 02:30:02 -0300

    Primeiro Commit já com layout pre-pronto

---
+++ classes/autenticador.php
@@ -0,0 +1,49 @@
+<?php
+        include_once "../db/db_config_mysql.php";
+        // Inicia sessões 
+        session_start();
+
+        if (!mysqli_connect_errno())
+        {
+                if( isset($_POST["usuario"]) && isset($_POST["password"]) )
+                {
+                        $usuario = $_POST["usuario"];
+                        $senha = md5($_POST["password"]);
+
+                        $sql_verifica_login = ("SELECT usuario_id, usuario, nome FROM usuarios WHERE usuario = '$usuario' " );
+                        $sql_verifica_password = ("SELECT usuario,senha FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'" );
+
+                        $checar_login = mysqli_query($conectar,$sql_verifica_login);
+                        $checar_password = mysqli_query($conectar,$sql_verifica_password);
+
+                        if (mysqli_num_rows($checar_login) == 0)
+                        {
+                            $_SESSION['menssagem'] = "Usuario inexistente!";
+                            header('Location: ../index.php');
+                            exit;
+                        }elseif (mysqli_num_rows($checar_password) == 0) {
+                            $_SESSION['menssagem'] = "Senha Incorreta!";
+                            header('Location: ../index.php');
+                            exit;
+                        }else{
+                            $dados = @mysqli_fetch_array($checar_login); 
+                            // TUDO OK! Agora, passa os dados para a sessão e redireciona o usuário 
+                            $_SESSION["id_usuario"]= $dados["usuario_id"]; 
+                            $_SESSION["nome_usuario"] = $dados["nome"]; 
+                            header('Location: ../ontRegister.php');
+                            exit;
+                        }
+
+                }
+                else
+                {
+                    $response["success"] = 0;
+                    $response["message"] = "Campo Faltando!";
+                    echo json_encode($response);
+                }
+        }else{
+                    $response["success"] = 0;
+                    $response["message"] = "Nao consegui entrar no servidor";
+                    echo json_encode($response);
+        }
+?>
