9c5ad66ea4321e3d89eb94b691c51dc3efaad421
Cruzverme <grossmino@hotmail.com>
2018-04-22 14:41:20 -0300

    Removido Icone de Config de Usuario de Index e Alterado permisao de pastas

--- index.php
+++ index.php
@@ -32,28 +32,6 @@
                 </button>
                 <a class="navbar-brand" href="index.html">Gerenciador ONT</a>
             </div>
-
-            <ul class="nav navbar-top-links navbar-right">
-
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li><a href="classes/logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-
-            </ul>
-
         <!-- /.navbar-header -->
         </nav>
 
