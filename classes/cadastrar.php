7309855c87855bf1a161e41adfb11ec45800746d
Cruzverme <grossmino@hotmail.com>
2018-04-23 01:48:40 -0300

    Ajustado Metodo de Cadastro

--- classes/cadastrar.php
+++ classes/cadastrar.php
@@ -1,58 +1,64 @@
 <?php
-        include_once "../db/db_config_mysql.php";
-        // Inicia sessões 
-        session_start();
+  include_once "../db/db_config_mysql.php";
+  // Inicia sessões 
+  session_start();
 
-        if (!mysqli_connect_errno())
-        {
-                if( isset($_SESSION["id_usuario"]) && isset($_POST["contrato"]) && isset($_POST["serial"]) && isset($_POST["cto"])
-                && isset($_POST["porta"]) && isset($_POST["numeroTel"]) && isset($_POST["numeroTel"]) && isset($_POST["passwordTel"])
-                && isset($_POST["telUser"]) && isset($_POST["pacote"]) )
-                {
-                       $usuario = $_SESSION["id_usuario"];
-                       $contrato = $_POST["contrato"];
-                       $serial = $_POST["serial"];
-                       $cto = $_POST["cto"];
-                       $porta = $_POST["porta"];
-                       $telNumber = $_POST["numeroTel"];
-                       $telPass = $_POST["passwordTel"];
-                       $telUser = $_POST["telUser"];
-                       $pacote = $_POST["pacote"];
-                    
-                        $sql_registra_onu = ("INSERT INTO ont (contrato, pon_mac, cto, tel_number, tel_user, tel_password, pacote, usuario_id, porta) 
-                                                VALUES ('$contrato','$serial','$cto','$telNumber','$telUser','$telPass','$pacote','$usuario','$porta')" );
+  if (!mysqli_connect_errno())
+  {
+      if( isset($_SESSION["id_usuario"]) && isset($_POST["contrato"]) && isset($_POST["serial"]) && isset($_POST["cto"])
+      && isset($_POST["porta"]) && isset($_POST["pacote"]) )
+      {
+            $usuario = $_SESSION["id_usuario"];
+            $contrato = $_POST["contrato"];
+            $serial = $_POST["serial"];
+            $cto = $_POST["cto"];
+            $porta = $_POST["porta"];
+            $pacote = $_POST["pacote"];
+            $telNumber = $_POST["numeroTel"];
+            $telPass = $_POST["passwordTel"];
+            $telUser = $_POST["telUser"];
 
-                        $cadastrar = mysqli_query($conectar,$sql_registra_onu);
-                        if ($cadastrar )               
-                        {
-                            $_SESSION['menssagem'] = "ONU Cadastrada!";
-                            header('Location: ../ontRegister.php');
-                            xit;
-                        }else{
-                            $_SESSION['menssagem'] = "ONU Não Cadastrada!";
-                            header('Location: ../ontRegister.php');
-                            exit;
-                        }
-                }
-                else
-                {
-                    $_SESSION['menssagem'] = "Campos Faltando!";
-                    header('Location: ../ontRegister.php');
-                    exit;
-                }
-        }else{
-            $_SESSION['menssagem'] = "Não Consegui Contato com Servidor!";
-            header('Location: ../ontRegister.php');
-            exit;
-        }
+            if(empty($telNumber) && empty($telPass) && empty($telUser) )
+            {
+                $telNumber = 0;
+                $telPass = 0;
+                $telUser = 0;
+            }
+          
+            $sql_registra_onu = ("INSERT INTO ont (contrato, pon_mac, cto, tel_number, tel_user, tel_password, pacote, usuario_id, porta) 
+                                    VALUES ('$contrato','$serial','$cto','$telNumber','$telUser','$telPass','$pacote','$usuario','$porta')" );
 
-        
-/*
-SQL PARA SALVAR NO RADIUS
-    INSERT INTO radcheck( username, attribute, op, value) VALUES ( '2500/13/0/485754439C96D58B@vertv', 'User-Name', ':=', '2500/13/0/485754430CEA4E9A@vertv' );
+            $cadastrar = mysqli_query($conectar,$sql_registra_onu);
+            if ($cadastrar )               
+            {
+                $_SESSION['menssagem'] = "ONU Cadastrada!";
+                header('Location: ../ont_register.php');
+                exit;
+            }else{
+                $_SESSION['menssagem'] = "ONU Não Cadastrada!";
+                header('Location: ../ont_register.php');
+                exit;
+            }
+      }
+      else
+      {
+          $_SESSION['menssagem'] = "Campos Faltando!";
+          header('Location: ../ont_register.php');
+          exit;
+      }
+  }else{
+      $_SESSION['menssagem'] = "Não Consegui Contato com Servidor!";
+      header('Location: ../ont_register.php');
+      exit;
+  }
 
-    INSERT INTO radcheck( username, attribute, op, value) VALUES ( '2500/13/0/485754439C96D58B@vertv', 'User-Password', ':=', ‘vlan’ );
 
-    INSERT INTO radcheck( username, attribute, op, value) VALUES ( '2500/13/0/485754439C96D58B@vertv', 'Huawei-Qos-Profile-Name', ':=', 'CORPF_10M' );
-*/
+  /*
+  SQL PARA SALVAR NO RADIUS
+  INSERT INTO radcheck( username, attribute, op, value) VALUES ( '2500/13/0/485754439C96D58B@vertv', 'User-Name', ':=', '2500/13/0/485754430CEA4E9A@vertv' );
+
+  INSERT INTO radcheck( username, attribute, op, value) VALUES ( '2500/13/0/485754439C96D58B@vertv', 'User-Password', ':=', ‘vlan’ );
+
+  INSERT INTO radcheck( username, attribute, op, value) VALUES ( '2500/13/0/485754439C96D58B@vertv', 'Huawei-Qos-Profile-Name', ':=', 'CORPF_10M' );
+  */
 ?>
