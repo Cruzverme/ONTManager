cd3f2236099c4ea887b8196b24c13fd81bb61265
Charles Pereira <Cruzverme@users.noreply.github.com>
2018-05-07 17:10:02 -0300

    Alterado Salvar CTOS

--- classes/cadastrar_cto.php
+++ classes/cadastrar_cto.php
@@ -5,24 +5,27 @@
 
   if (!mysqli_connect_errno())
   {
-    if( isset($_POST["cto"]) && !empty($_POST["cto"]) && isset($_POST["porta"]) && !empty($_POST["porta"]) )
+    if( isset($_POST["cto"]) && !empty($_POST["cto"]) && isset($_POST["porta"]) && !empty($_POST["porta"])
+        && isset($_POST["pon"]) )
     {
         $cto = $_POST["cto"];
         $porta_atendimento = $_POST["porta"];
+        $pon = $_POST["pon"];
         
         for($portas = 1; $portas <= $porta_atendimento; $portas++)
         {
-          $sql_insere_caixa = ("INSERT INTO ctos(caixa_atendimento,porta_atendimento) VALUES('$cto',$portas)");
+          $sql_insere_caixa = ("INSERT INTO ctos(caixa_atendimento,porta_atendimento,slot_pon) VALUES('$cto',$portas,'$pon')");
           $checar_insert = mysqli_query($conectar,$sql_insere_caixa);
         }
 
         if($checar_insert)
         { 
-             echo  $_SESSION['menssagem'] = "Caixa Registrada!";
-              header('Location: ../cto_classes/cto_create.php');
-              mysqli_close($conectar);
-              exit;
+            echo  $_SESSION['menssagem'] = "Caixa Registrada!";
+            header('Location: ../cto_classes/cto_create.php');
+            mysqli_close($conectar);
+            exit;
         }else{
+          echo $pon;
           $erro = mysqli_error($conectar);
           $_SESSION['menssagem'] = "CTO Não Cadastrada! SQL: $erro";
           header('Location: ../cto_classes/cto_create.php');
