0c2e3ded048f5d68129f7f24e30a84b83d1edc2a
Charles Pereira <Cruzverme@users.noreply.github.com>
2018-05-15 09:06:00 -0300

    Ajustado Deletar ONT

--- classes/deletar.php
+++ classes/deletar.php
@@ -12,52 +12,52 @@
       $contrato = $_POST["contrato"];
       $serial = $_POST["serial"];
 
-      
-      $sql_disponibiliza_porta = "UPDATE ctos SET porta_atendimento_disponivel = 1 WHERE serial = '$serial'";
+      // disponibiliza a porta para seleção
+      $sql_disponibiliza_porta = "UPDATE ctos SET porta_atendimento_disponivel = 0 WHERE serial = '$serial'";
       $executa_query = mysqli_query($conectar,$sql_disponibiliza_porta);
 
-      if ($executa_query) 
+      if ($executa_query)
       {
-        $deletar_onu_radius_banda = "DELETE FROM radreply WHERE username='2500/13/0/$serial@vertv' 
-          AND attribute='Huawei-Qos-Profile-Name' ";
-        $executa_query= mysqli_query($conectar_radius,$deletar_onu_radius_banda);
+         $deletar_onu_radius_banda = "DELETE FROM radreply WHERE username='2500/13/0/$serial@vertv' 
+           AND attribute='Huawei-Qos-Profile-Name' ";
+         $executa_query= mysqli_query($conectar_radius,$deletar_onu_radius_banda);
 
-        $deletar_onu_radius = " DELETE FROM radcheck WHERE username='2500/13/0/$serial@vertv' ";
-        $executa_query_radius = mysqli_query($conectar_radius,$deletar_onu_radius);
+         $deletar_onu_radius = " DELETE FROM radcheck WHERE username='2500/13/0/$serial@vertv' ";
+         $executa_query_radius = mysqli_query($conectar_radius,$deletar_onu_radius);
 
-        if ($executa_query && $deletar_onu_radius) 
-        {
-          $sql_apagar_onu = ("DELETE FROM ont WHERE contrato = '$contrato' AND serial = '$serial'" );
+         if ($executa_query && $deletar_onu_radius) 
+         {
+           $sql_apagar_onu = ("DELETE FROM ont WHERE contrato = '$contrato' AND serial = '$serial'" );
 
-          $deletar_onu = mysqli_query($conectar,$sql_apagar_onu);
-          if($deletar_onu)
-          {
-              if ( $total = mysqli_affected_rows($conectar))    //retorna quantas rows foram afetadas           
-              {
-                  $_SESSION['menssagem'] = "$total ONU Removida!";
-                  header('Location: ../ont_classes/ont_delete.php');
-                  mysqli_close($conectar);
-                  exit;
-              }else{
-                  $_SESSION['menssagem'] = "ONU Não Removida!";
-                  header('Location: ../ont_classes/ont_delete.php');
-                  mysqli_close($conectar);
-                  exit;
-              }
-          }else{
-              $erro = mysqli_error($conectar);
-              $_SESSION['menssagem'] = "Houve erro ao deletar SQL: $erro";
-              header('Location: ../ont_classes/ont_delete.php');
-              mysqli_close($conectar);
-              exit;
-          }
-        }else{
-          $erro = mysqli_error($conectar);
-          $_SESSION['menssagem'] = "Houve erro ao deletar SQL Radius: $erro";
-          header('Location: ../ont_classes/ont_delete.php');
-          mysqli_close($conectar);
-          exit;
-        }
+           $deletar_onu = mysqli_query($conectar,$sql_apagar_onu);
+           if($deletar_onu)
+           {
+               if ( $total = mysqli_affected_rows($conectar))   // retorna quantas rows foram afetadas           
+               {
+                   $_SESSION['menssagem'] = "$total ONU Removida!";
+                   header('Location: ../ont_classes/ont_delete.php');
+                   mysqli_close($conectar);
+                   exit;
+               }else{
+                   $_SESSION['menssagem'] = "ONU Não Removida!";
+                   header('Location: ../ont_classes/ont_delete.php');
+                   mysqli_close($conectar);
+                   exit;
+               }
+           }else{
+               $erro = mysqli_error($conectar);
+               $_SESSION['menssagem'] = "Houve erro ao deletar SQL: $erro";
+               header('Location: ../ont_classes/ont_delete.php');
+               mysqli_close($conectar);
+               exit;
+           }
+         }else{
+           $erro = mysqli_error($conectar);
+           $_SESSION['menssagem'] = "Houve erro ao deletar SQL Radius: $erro";
+           header('Location: ../ont_classes/ont_delete.php');
+           mysqli_close($conectar);
+           exit;
+         }
       }else{
         $erro = mysqli_error($conectar);
         $_SESSION['menssagem'] = "Houve erro ao deletar SQL: $erro";
