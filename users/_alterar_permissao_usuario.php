fca9a31a9b2d952dc9dfb2d985e3a7dbc7ae82ad
Charles Pereira <Cruzverme@users.noreply.github.com>
2018-06-11 11:07:06 -0300

    Ajustado Tamanho de Formulario

--- users/_alterar_permissao_usuario.php
+++ users/_alterar_permissao_usuario.php
@@ -49,7 +49,7 @@
   <div id="page-wrapper">
     <div class="container">
       <div class="row">
-        <div class="col-md-11 col-md-offset-0">
+        <div class="col-md-4 col-md-offset-2">
           <div class="login-panel panel panel-default">
             <div class="panel-heading">
               <h3 class="panel-title">Editar Usuário <?php echo $nome; ?></h3>
