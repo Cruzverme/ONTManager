a168422b1496b2c44da96cbcad3969964e01997d
Cruzverme <grossmino@hotmail.com>
2018-06-18 19:32:11 -0300

    Corrigido Bug de Nao Mostrar PON

--- cto_classes/cto_create.php
+++ cto_classes/cto_create.php
@@ -39,28 +39,49 @@
                               <label>PON</label> 
                               <select class="form-control" name="pon">
                                 <?php 
-                                  $sql_consulta_serial = "SELECT DISTINCT olt.frame,olt.slot,olt.porta FROM pon olt 
-                                    INNER JOIN ctos cto ON cto.pon_id_fk = $olt
-                                    WHERE olt.pon_id = $olt";//"SELECT frame,slot,porta FROM pon WHERE pon_id = $olt";
+                                   $sql_check = "SELECT * FROM ctos WHERE pon_id_fk = $olt";
+                                   $executa_check = mysqli_query($conectar,$sql_check);
+                                   if(mysqli_num_rows($executa_check) > 0)
+                                   {
+                                      $sql_consulta_serial = "SELECT DISTINCT olt.frame,olt.slot,olt.porta FROM pon olt 
+                                        INNER JOIN ctos cto ON cto.pon_id_fk = $olt
+                                        WHERE olt.pon_id = $olt";//"SELECT frame,slot,porta FROM pon WHERE pon_id = $olt";
+                                      
+                                      $array_ctos = array();
 
-                                  $array_ctos = array();
+                                      $executa_query = mysqli_query($conectar,$sql_consulta_serial) or die(mysqli_error($conectar));
 
-                                  $executa_query = mysqli_query($conectar,$sql_consulta_serial) or die(mysqli_error($conectar));
-                                  $sql_get_fsp = "SELECT DISTINCT caixa_atendimento,frame_slot_pon FROM `ctos` WHERE pon_id_fk = $olt";
-                                  $executa_get_fsp = mysqli_query($conectar,$sql_get_fsp);
+                                      $sql_get_fsp = "SELECT DISTINCT caixa_atendimento,frame_slot_pon FROM `ctos` WHERE pon_id_fk = $olt";
+                                      $executa_get_fsp = mysqli_query($conectar,$sql_get_fsp);
 
-                                  while ($porta_pon_cadastrada = mysqli_fetch_array($executa_get_fsp,MYSQLI_BOTH))
-                                  {
-                                    array_push($array_ctos,$porta_pon_cadastrada['frame_slot_pon']);
-                                  }
-                                  while ($ont = mysqli_fetch_array($executa_query, MYSQLI_BOTH))
-                                  {
-                                    for($porta = 0;$porta < $ont['porta'];$porta++)
-                                    {
-                                        if(!in_array("$ont[frame]-$ont[slot]-$porta",$array_ctos))
-                                            echo "<option value=$olt-$ont[frame]-$ont[slot]-$porta> Slot: $ont[slot]  Porta: $porta </option>";
+                                      while ($porta_pon_cadastrada = mysqli_fetch_array($executa_get_fsp,MYSQLI_BOTH))
+                                      {
+                                        array_push($array_ctos,$porta_pon_cadastrada['frame_slot_pon']);
+                                      }
+                                      while ($ont = mysqli_fetch_array($executa_query, MYSQLI_BOTH))
+                                      {
+                                        for($porta = 0;$porta < $ont['porta'];$porta++)
+                                        {
+                                            if(!in_array("$ont[frame]-$ont[slot]-$porta",$array_ctos))
+                                                echo "<option value=$olt-$ont[frame]-$ont[slot]-$porta> Slot: $ont[slot]  Porta: $porta </option>";
+                                        }
+                                      }
+                                    }else{
+                                      $sql_consulta_serial = "SELECT frame, slot, porta FROM pon WHERE pon_id = $olt";
+
+                                      $executa_query = mysqli_query($conectar,$sql_consulta_serial) or die(mysqli_error($conectar));
+                                      
+                                      while ($ont = mysqli_fetch_array($executa_query, MYSQLI_BOTH))
+                                      {
+                                        for($porta = 0;$porta < $ont['porta'];$porta++)
+                                        {
+                                            //if(!in_array("$ont[frame]-$ont[slot]-$porta",$array_ctos))
+                                                echo "<option value=$olt-$ont[frame]-$ont[slot]-$porta> Slot: $ont[slot]  Porta: $porta </option>";
+                                        }
+                                      }
+
+
                                     }
-                                  }
                                 ?>
                               </select>
                             </div>
