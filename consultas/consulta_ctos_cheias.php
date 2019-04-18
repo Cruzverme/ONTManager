683273bf3901cf766886e2298e2b4759f2d8d4e8
Cruzverme <grossmino@hotmail.com>
2019-04-18 11:08:32 -0300

    Corrigido Bug de Path

--- consultas/consulta_ctos_cheias.php
+++ consultas/consulta_ctos_cheias.php
@@ -1,14 +1,14 @@
 <?php 
 
-  include_once "../db/db_config_mysql.php";
+  include_once "/var/www/html/ontManager/db/db_config_mysql.php";
   
   ini_set('display_errors', 1);
 
   error_reporting(E_ALL);
 
-  $from = "abobrinha@vertv.com.br";
+  $from = "monitoramento@vertv.com.br";
 
-  $to = "charles@vertv.com.br";
+  $to = "ti@vertv.com.br,rede@vertv.com.br";
 
   $subject = "CTOs No Limite de Portas Disponíveis";
 
@@ -16,7 +16,7 @@
 
   $sql_get_qtd_porta_cto_disponivel = "SELECT count(porta_atendimento_disponivel),caixa_atendimento,frame_slot_pon 
                                         FROM `ctos` WHERE porta_atendimento_disponivel = 0 
-                                        GROUP BY porta_atendimento_disponivel,caixa_atendimento,frame_slot_pon 
+                                        GROUP BY  porta_atendimento_disponivel,caixa_atendimento,frame_slot_pon 
                                         order by caixa_atendimento";
 
   $executa_sql_get_qtd_porta_cto_disponivel = mysqli_query($conectar,$sql_get_qtd_porta_cto_disponivel);
@@ -25,23 +25,12 @@
 
   while($row = mysqli_fetch_array($executa_sql_get_qtd_porta_cto_disponivel,MYSQLI_BOTH))
   {
-    if($row[0] <= 1)
-      $estilo = "style='color:red;'";
-    elseif($row[0] == 2)
-      $estilo = "style='color:yellow;'";
-    else
-      $estilo = '';
-    
-    array_push($mensagemCTOCritica,"<p $estilo>CTO: $row[1] Quantidade Disponivel: $row[0] Frame-Slot-Pon: $row[2] </p>"); 
+    if($row[0] <= 2) 
+      array_push($mensagemCTOCritica,"CTO: $row[1] Quantidade Disponivel: $row[0] Frame-Slot-Pon: $row[2] \r\n ");
   }
-
-  $message = "$mensagemCTOCritica";
+  $implo = implode('',$mensagemCTOCritica);
+  $message = $implo;
   
-  //mail($to, $subject, $message, $headers);
-
-  echo "A mensagem de e-mail foi enviada.";
-
-
-
+  mail($to, $subject, $message, $headers);
 
 ?>
