cd2698aea5aeb6dbd216f5c1633a0290f88c8e77
Cruzverme <grossmino@hotmail.com>
2019-05-02 16:41:56 -0300

    Ajustado Consulta de CTOs Cheias

--- consultas/consulta_ctos_cheias.php
+++ consultas/consulta_ctos_cheias.php
@@ -15,7 +15,7 @@
   $headers = "De:". $from;  
 
   $sql_get_qtd_porta_cto_disponivel = "SELECT count(porta_atendimento_disponivel),caixa_atendimento,frame_slot_pon 
-                                        FROM `ctos` WHERE porta_atendimento_disponivel = 0 
+                                        FROM `ctos` WHERE porta_atendimento_disponivel = 1 
                                         GROUP BY  porta_atendimento_disponivel,caixa_atendimento,frame_slot_pon 
                                         order by caixa_atendimento";
 
@@ -25,8 +25,11 @@
 
   while($row = mysqli_fetch_array($executa_sql_get_qtd_porta_cto_disponivel,MYSQLI_BOTH))
   {
-    if($row[0] <= 2) 
-      array_push($mensagemCTOCritica,"CTO: $row[1] Quantidade Disponivel: $row[0] Frame-Slot-Pon: $row[2] \r\n ");
+    if($row[0] >= 6) 
+    {
+      $quantidade_restante = 8 - $row[0];
+      array_push($mensagemCTOCritica,"CTO: $row[1] Quantidade Disponivel: $quantidade_restante Frame-Slot-Pon: $row[2] \r\n ");
+    }
   }
   $implo = implode('',$mensagemCTOCritica);
   $message = $implo;
