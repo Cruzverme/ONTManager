6c3845fd5f93322a27cb03cc91987a91ec8ba9b8
Charles Pereira <Cruzverme@users.noreply.github.com>
2019-06-13 17:01:43 -0300

    Add Vlan Implemented

---
+++ vlan/add_vlan.php
@@ -0,0 +1,66 @@
+<?php 
+  include_once "../classes/html_inicio.php";
+  include_once "../db/db_config_mysql.php";
+
+  if($_SESSION["modificar_onu"] == 0) {
+    echo '
+    <script language= "JavaScript">
+      alert("Sem Permissão de Acesso!");
+      location.href="../classes/redirecionador_pagina.php";
+    </script>
+    ';
+  }
+  
+  $olt = filter_input(INPUT_POST,"olt");
+  //list($olt_id, $deviceName) = explode("-",$olt);
+
+  
+
+  #$sql_ctos =  "SELECT DISTINCT caixa_atendimento,frame_slot_pon FROM ctos WHERE pon_id_fk = $olt_id";
+  #$executa_query = mysqli_query($conectar,$sql_ctos);
+
+  // while ($ctos = mysqli_fetch_array($executa_query, MYSQLI_ASSOC))
+  // {
+  //   $caixa = $ctos['caixa_atendimento'];
+  //   $cto = $ctos['frame_slot_pon'];
+  // }
+?>
+  <div id="page-wrapper">
+    <!-- <div class="container"> -->
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="login-panel panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Cadastro de Vlan</h3>
+            </div>
+            <div class="panel-body">
+              <form action="../classes/create_vlan.php" method="post">
+                <div class='form-group'>
+                  <label for="device">OLT</label>
+                  <input type="text" class='form-control' name="deviceName" id="device" readonly value=<?php echo $olt; ?> />
+                </div>
+
+                <div class='form-group'>
+                  <label for="vlan">Numero da Vlan</label>
+                  <input type="number" class='form-control' name="nVlan" id="vlan"/>
+                </div>
+
+                <div class='form-group'>
+                  <label for="descricao">Descrição da Vlan</label>
+                  <input type="text" class='form-control' name="aliasVlan" id="descricao"/>
+                </div>
+
+                <div class='form-group'>
+                  <button class='btn form-control'>Criar Vlan</button>
+                </div>
+              </form>  
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- </div> -->
+  </div>
+
+
+
+<?php include_once "../classes/html_fim.php";   ?>
---
+++ vlan/select_olt.php
@@ -0,0 +1,48 @@
+<?php 
+  include_once "../classes/html_inicio.php";
+  include_once "../db/db_config_mysql.php";
+
+  if($_SESSION["modificar_onu"] == 0) {
+    echo '
+    <script language= "JavaScript">
+      alert("Sem Permissão de Acesso!");
+      location.href="../classes/redirecionador_pagina.php";
+    </script>
+    ';
+  }
+?>
+  <div id="page-wrapper">
+    <!-- <div class="container"> -->
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="login-panel panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Cadastro de Vlan</h3>
+            </div>
+            <div class="panel-body">
+              <form role="form" action="./add_vlan.php" method="post">
+                <div class="form-group">
+                  <label>Selecione a OLT</label>
+                  <select class="form-control" name="olt">
+                    <?php 
+                      $sql_consulta_olt = "SELECT deviceName,pon_id,slot FROM pon";
+                      $executa_query = mysqli_query($conectar,$sql_consulta_olt);
+                      while ($ont = mysqli_fetch_array($executa_query, MYSQLI_BOTH))
+                      {
+                        echo "<option value=$ont[deviceName]>$ont[deviceName] - Slot: $ont[slot]</option>";
+                      }
+                    ?>
+                  </select>
+                </div>
+                <button class="btn btn-lg btn-success btn-block">Avançar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- </div> -->
+  </div>
+
+
+
+<?php include_once "../classes/html_fim.php";   ?>
