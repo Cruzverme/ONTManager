ed36c4571c622d91ab8467848f71241bb06eaf17
Cruzverme <grossmino@hotmail.com>
2019-09-03 18:55:12 -0300

    Moved to better local

---
+++ consultas/js/vertv_interno.js
@@ -0,0 +1,44 @@
+function bloquear(contrato,serial){
+  $.post("../classes/gerencia_bloqueios.php",{motivo: 2,contrato,serial},function(msg){
+    if(msg == "Cliente desativado")
+    {
+      alert(msg);
+      window.location.reload();
+    }else{
+      alert(msg);
+    }
+  });
+}
+
+function desbloquear(contrato,serial){
+  $.post("../classes/gerencia_bloqueios.php",{motivo: 1,contrato,serial},function(msg){
+    if(msg == "Cliente reativado")
+    {
+      alert(msg);
+      window.location.reload();
+    }else{
+      alert(msg);
+    }
+  });
+}
+
+function verificar_inadimplente_erp()
+{
+  var body = $('#page-wrapper');
+
+  $(document).on({
+    ajaxStart: function() {body.addClass("loading");}
+  });
+
+  $.post("../classes/verifica_pendencia_pagamento.php",function(msg){
+    if(msg == "concluido")
+    {  
+      body.removeClass("loading");
+      alert(msg);
+      window.location.reload();
+    }else{
+      body.removeClass("loading");
+      alert(msg);
+    }
+  })
+}
