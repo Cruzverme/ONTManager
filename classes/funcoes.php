dcc1d7808592e20130d225e26fb4f96778862385
Cruzverme <grossmino@hotmail.com>
2019-09-06 12:01:44 -0300

    Add PHPMailer and SendMail Function

--- classes/funcoes.php
+++ classes/funcoes.php
@@ -486,4 +486,95 @@
     return $result;
   }
 
+  function send_email($assunto,$corpoEmail,$destinatario,$nomeDestinatario,$arquivo = NULL)
+  {
+    include "../auth/autenticacoes.php";
+    
+    // Inclui o arquivo class.phpmailer.php localizado na mesma pasta do arquivo php 
+    include "../lib/mailer/PHPMailerAutoload.php";
+
+    // Inicia a classe PHPMailer 
+    $mail = new PHPMailer(); 
+    
+    // Método de envio 
+    $mail->IsSMTP(); 
+    
+    // Enviar por SMTP 
+    $mail->Host = $host_email;
+
+    // Você pode alterar este parametro para o endereço de SMTP do seu provedor 
+    $mail->Port = $snmp_port; 
+    
+    // Usar autenticação SMTP (obrigatório) 
+    $mail->SMTPAuth = true;
+
+    // Usuário do servidor SMTP (endereço de email) 
+    // obs: Use a mesma senha da sua conta de email 
+    $mail->Username = $usuario_email; 
+    $mail->Password = $senha_email; 
+
+    // Configurações de compatibilidade para autenticação em TLS 
+    $mail->SMTPOptions = array( 'ssl' => array( 'verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true ) ); 
+
+    // Você pode habilitar esta opção caso tenha problemas. Assim pode identificar mensagens de erro. 
+    // $mail->SMTPDebug = 2; 
+    
+    // Define o remetente 
+    // Seu e-mail 
+    $mail->From = "bloqueadorcliente@vertv.com.br"; 
+    
+    // Seu nome 
+    $mail->FromName = "VERTV"; 
+    
+    // Define o(s) destinatário(s) 
+    $mail->AddAddress('ti@vertv.com.br', 'TI');
+
+    // Opcional: mais de um destinatário
+    // $mail->AddAddress('fernando@email.com'); 
+    
+    // Opcionais: CC e BCC
+    // $mail->AddCC('joana@provedor.com', 'Joana'); 
+    // $mail->AddBCC('roberto@gmail.com', 'Roberto'); 
+    
+    // Definir se o e-mail é em formato HTML ou texto plano 
+    // Formato HTML . Use "false" para enviar em formato texto simples ou "true" para HTML.
+    $mail->IsHTML(true); 
+    
+    // Charset (opcional) 
+    $mail->CharSet = 'UTF-8'; 
+    
+    // Assunto da mensagem 
+    $mail->Subject = "O Céu está caindo"; 
+    
+    // Corpo do email 
+    $mail->Body = '<table>
+                      <tr><th>vai</th></tr>
+                      <tr><td>Pokebola</td></tr>
+                  </table>
+                  
+                  <div>
+                    <h3>Mais um Teste de Pokemon</h3>
+                  </div>'; 
+    
+    if($arquivo != NULL)
+    {
+      // Opcional: Anexos 
+      $mail->AddAttachment("/home/charles/Downloads/cdr__1567166382192.168.80.6.csv", "documento.csv");
+    }
+
+    // Envia o e-mail 
+    $enviado = $mail->Send(); 
+    
+    // Exibe uma mensagem de resultado 
+    if ($enviado) 
+      echo "Seu email foi enviado com sucesso!"; 
+    else
+      echo "Houve um erro enviando o email: ".$mail->ErrorInfo;
+  }
+
+  function create_xls()
+  {
+
+  }
+
 ?>
