3da0eaf95c7ac63a6c44df13f2e725ca7fa13775
Cruzverme <grossmino@hotmail.com>
2019-11-21 14:06:18 -0300

    New IP Works

--- classes/cadastrar_ip.php
+++ classes/cadastrar_ip.php
@@ -8,11 +8,40 @@
   
   if( $ipInicial && $ipFinal)
   {
+
+    $inicial = explode('.',$ipInicial);
+    $final = explode('.',$ipFinal);
+
     #LAST IP DONT MUST BE SMALLER THAN INITIAL IP
-    $ipFinal < $ipInicial? die('Ip Final Não Pode Ser Menor que a Inicial') : '';
-    echo "VOU CADASTRAR SEU INUTIL!";
+    $inicial[3] > $final[3]? die("Ip Final Não Pode Ser Menor que a Inicial") : '';
+
+    if(($inicial[0] == $final[0]) && ($inicial[1] == $final[1]) && ($inicial[2] == $final[2]) )
+    {
+      $ip = $inicial[3];
+      $naoInseridos = array();
+      $total = 0;
+      while($inicial[3] <= $final[3])
+      {
+        $ipReal = "$inicial[0].$inicial[1].$inicial[2].$ip";
+
+        $sqlInsertIp = "INSERT INTO ips_valido SET numero_ip = '$ipReal'";
+        $execute = mysqli_query($conectar,$sqlInsertIp);
+        
+        if(mysqli_errno($conectar) == 1062)
+        {
+          array_push($naoInseridos,$ipReal);
+        }
+        $ip = $inicial[3]+=1;
+        $total+=1;
+      }
+      $totalNaoInseridos = count($naoInseridos);
+      
+      $totalNaoInseridos > 0? $msg = "$totalNaoInseridos de $total IPs não foram inseridos devido já estarem cadastrados!" : $msg = "$ipInicial até $ipFinal foram cadastrados!";
+      
+      echo $msg;
+    }  
+    else
+      echo "A Classe de IP Está diferente, favor corrigir!";
   }else{
     die('Está faltando definir o range de IP');
   }
-    
-
